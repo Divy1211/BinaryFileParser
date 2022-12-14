c9eaa19d031b538a3c49a13c9680f51652bbf9dd
Alian713 <divy1211.dc@gmail.com>
2022-12-14 18:17:06 +0530

    Updated readme

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from .errors.CompressionError import CompressionError
+from .errors.ParserError import ParserError
+from .errors.VersionError import VersionError
+from .retrievers.BaseStruct import BaseStruct
+from .retrievers.Retriever import Retriever
