b082826a7a7ab48ac6b7e7fe1cc9760647b2d6e2
Alian713 <divy1211.dc@gmail.com>
2023-01-31 00:47:09 +0530

    Added aoe2scenario v1.46 support

--- testing/test.py
+++ testing/test.py
@@ -2,8 +2,9 @@ from testing.sections.ScenarioSections import ScenarioSections
 
 
 def main():
-    scx = ScenarioSections.from_file("1_47.aoe2scenario")
-    scx.to_file("first.aoe2scenario")
+    scx = ScenarioSections.from_file("1_46.aoe2scenario")
+    print(scx.trigger_data.num_triggers)
+    scx.to_file("first_1_46.aoe2scenario")
 
 if __name__ == "__main__":
     main()
