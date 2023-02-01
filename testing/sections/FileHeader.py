017f5ce9c8949c3c0e7b3cc62d8ff6987033100a
Alian713 <divy1211.dc@gmail.com>
2023-02-01 21:40:02 +0530

    savable

--- testing/sections/FileHeader.py
+++ testing/sections/FileHeader.py
@@ -9,9 +9,14 @@ class FileHeader(BaseStruct):
     def update_num_triggers(retriever: Retriever, instance: FileHeader):
         instance.num_triggers = len(instance.parent.trigger_data.triggers)
 
+    @staticmethod
+    def set_timestamp_of_last_save_repeat(retriever: Retriever, instance: FileHeader):
+        if instance.savable < 2:
+            FileHeader.timestamp_of_last_save.set_repeat(instance, 0)
+
     file_version: str = Retriever(FixedLenStr[4], default = "1.47")
     header_len: int = Retriever(uint32, default = 0)
-    savable: int = Retriever(int32, default = 6)
+    savable: int = Retriever(int32, default = 6, on_set = [set_timestamp_of_last_save_repeat])
     timestamp_of_last_save: int = Retriever(uint32, default = 1610675127)
     scenario_instructions: str = Retriever(nt_str32, default = "")
     individual_victories_used: bool = Retriever(bool32, default = False, max_ver = (1, 37))
@@ -21,7 +26,7 @@ class FileHeader(BaseStruct):
     unknown2: int = Retriever(uint32, default = 1)
     """always (?) 1"""
     unknowns: list[int] = Retriever(Array32[uint32], default = [2, 3, 4, 5, 6, 7])
-    creator: str = Retriever(nt_str32, default = "AoE2ScenarioParser")
+    creator: str = Retriever(nt_str32, default = "AoE2SP")
     num_triggers: int = Retriever(uint32, default = 0, on_write = [update_num_triggers])
 
     def __init__(self, struct_version: tuple[int, ...] = (1, 47), parent: BaseStruct = None, initialise_defaults = True):
