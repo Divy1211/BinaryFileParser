11c98e8e44dace15ce89541e7ea45e6ad9f788a8
Alian713 <divy1211.dc@gmail.com>
2023-04-18 20:22:40 +1000

    Added sub retrievers to retriever module init

--- src/binary_file_parser/retrievers/__init__.py
+++ src/binary_file_parser/retrievers/__init__.py
@@ -1,3 +1,5 @@
 from .BaseStruct import BaseStruct
 from .MapValidate import MapValidate
 from .Retriever import Retriever
+from .RetreiverRef import RetrieverRef
+from .RetreiverCombiner import RetrieverCombiner
