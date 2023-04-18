fec1bfa4f777e743ce807737b82ccc09d44561c3
Alian713 <divy1211.dc@gmail.com>
2023-04-18 22:34:21 +1000

    Added docstr for Version

--- src/binary_file_parser/utils.py
+++ src/binary_file_parser/utils.py
@@ -1,4 +1,8 @@
 class Version(tuple[int, ...]):
+    """
+    - Functions equivalently to ``tuple[int, ...]``, e.g.: ``Version((1, 2, 3))`` is exactly the same as ``(1, 2, 3)``
+    - Exists solely for custom ``str()`` and ``repr()`` so ``Version((1, 2, 3))`` is printed as ``v1.2.3``
+    """
     def __repr__(self):
         return f"Version({super().__repr__()})"
 
