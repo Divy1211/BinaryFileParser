176517e43eee628621828e91927df37a658a18e2
Divy <divy1211.dc@gmail.com>
2025-04-04 15:16:26 +0800

    Added mutability error to nav

--- stubs/bfp_rs/errors/mutability_test.pyi
+++ stubs/bfp_rs/errors/mutability_test.pyi
@@ -1,4 +1,6 @@
 class MutabilityError(ValueError):
     """
-    Thrown when a list is set as immutable and if mutation is attempted
+    Thrown when a list is set as immutable and if mutation is attempted.
+
+    See Also: ``set_mut`` and ``borrow_mut``
     """
