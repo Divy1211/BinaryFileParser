c38eda187d2120887596317268fc71da8f082caf
Divy <divy1211.dc@gmail.com>
2025-04-04 15:34:45 +0800

    Added usage to borrow mut, removed list return type

--- stubs/bfp_rs/mut.pyi
+++ stubs/bfp_rs/mut.pyi
@@ -3,21 +3,24 @@ import contextlib
 
 def set_mut(ls: list, val: bool):
     """
-    Set the mutability for a list
+    Set the mutability for a list.
 
     Args:
         ls: The list
-        val: If set to ``False``, disallow methods like ``append`` or ``extend``
+        val: If set to ``False``, disallow methods like ``append`` or ``extend`` on the provided list
     """
 
 @contextlib.contextmanager
-def borrow_mut(ls: list) -> list:
+def borrow_mut(ls: list):
     """
-    Temporarily allow mutating a list
+    Context manager to temporarily allow mutating a list.
+
+    Usage:
+        ```
+        with borrow_mut(struct.ls):
+            struct.ls.append(...)
+        ```
 
     Args:
         ls: The list
-
-    Returns:
-
     """
