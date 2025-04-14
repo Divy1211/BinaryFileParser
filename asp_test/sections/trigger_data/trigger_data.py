60798cb2a867c8fee96920e9d76e97aad8d6cfa0
Divy <divy1211.dc@gmail.com>
2025-04-14 20:29:19 +0800

    v1.55

--- asp_test/sections/trigger_data/trigger_data.py
+++ asp_test/sections/trigger_data/trigger_data.py
@@ -2,13 +2,15 @@ from __future__ import annotations
 
 from bfp_rs import BaseStruct, Retriever, Version, ByteStream, ret
 from bfp_rs.types.le import (
-    Array32, f64, i8, u32, Bytes, bool8, u64,
+    Array32, f64, i8, u32, bool8, u64, Array64,
 )
 from bfp_rs.combinators import set_repeat
 
 from asp_test.sections.scx_versions import TRIGGER_LATEST
 from asp_test.sections.trigger_data.trigger import Trigger
 from asp_test.sections.trigger_data.variable_data import VariableData
+from asp_test.sections.trigger_data.decision import Decision
+from asp_test.sections.trigger_data.attacker import Attacker
 
 
 def trigger_display_orders_repeat():
@@ -27,6 +29,8 @@ class TriggerData(BaseStruct):
     unused2: int                       = Retriever(u32,                       min_ver = Version(2, 7), default = 0)
     unused3: int                       = Retriever(bool8,                     min_ver = Version(2, 7), default = False)
     unused4: bytes                     = Retriever(u64,                       min_ver = Version(3, 5), default = 0)
+    decisions: Decision                = Retriever(Array64[Decision],         min_ver = Version(4, 0), default_factory = lambda _: []) #, remaining_compressed = True)
+    attackers: Attacker                = Retriever(Array64[Attacker],         min_ver = Version(4, 1), default_factory = lambda _: [])
     # @formatter:on
 
     @classmethod
--- asp_test/test.py
+++ asp_test/test.py
@@ -1,18 +1,45 @@
+import os
 import sys
+
+from bfp_rs import Version
 
 # drive = "C:"
 # drive = "/mnt/c"
 
-sys.path.insert(0, r"C:/Users/Divy/PycharmProjects/bfp-rs")
+# sys.path.insert(0, r"C:/Users/Divy/PycharmProjects/bfp-rs")
 # sys.path.insert(0, r"/mnt/c/Users/Divy/PycharmProjects/bfp-rs/")
 
 from asp_test.sections import ScenarioSections
 from asp_test.utils import timed
 
 with timed("read"):
-    scx: ScenarioSections = ScenarioSections.from_file(r"C:\Users\Divy\PycharmProjects\BinaryFileParser\asp_test\scxs\Bomberman 8p_1_47.aoe2scenario")
-    # scx: ScenarioSections = ScenarioSections.from_file(r"/mnt/c/Users/Divy/PycharmProjects/BinaryFileParser/asp_test/scxs/Bomberman 8p_1_47.aoe2scenario")
-    print(scx.file_header.ver)
+    scx = ScenarioSections.from_file(r"C:\Users\dchandra\Games\Age of Empires 2 DE\76561199834870459\resources\_common\scenario\test_155_lc.aoe2scenario")
 
-# with timed("write"):
-#     ScenarioSections.to_file("./wtest.bin", scx)
+    # # scx: ScenarioSections = ScenarioSections.from_file(r"C:\Users\Divy\PycharmProjects\BinaryFileParser\asp_test\scxs\Bomberman 8p_1_47.aoe2scenario")
+    # # scx: ScenarioSections = ScenarioSections.from_file(r"C:\Users\dchandra\Games\Age of Empires 2 DE\76561199834870459\resources\_common\scenario\food_units.aoe2scenario")
+    # # scx: ScenarioSections = ScenarioSections.from_file(r"C:\Users\dchandra\Games\Age of Empires 2 DE\76561199834870459\resources\_common\scenario\test.aoe2scenario", strict = False)
+    # # ScenarioSections.to_file(r"C:\Users\dchandra\Games\Age of Empires 2 DE\76561199834870459\resources\_common\scenario\test_w.aoe2scenario", scx)
+    #
+    # path = r"C:\Users\dchandra\Games\Age of Empires 2 DE\76561199834870459\resources\_common\scenario"
+    #
+    # for file in os.listdir(path):
+    #     name = os.path.join(path, file)
+    #
+    #     try:
+    #         scx: ScenarioSections = ScenarioSections.from_file(name)
+    #     except ValueError:
+    #         print("error: ", name)
+    #         continue
+    #
+    #     print(file, scx.ver)
+    #
+    #     if scx.ver != Version(1, 55):
+    #         continue
+    #
+    #     scx.trigger_data.version = 3.9
+    #     scx.file_header.file_version = '1.54'
+    #
+    #     name_out = os.path.join(path, "..", "scenario_154", file)
+    #
+    #     ScenarioSections.to_file(name_out, scx)
+    #     print(name_out)
